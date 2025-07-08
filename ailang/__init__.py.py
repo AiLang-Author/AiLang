d57a68485707dad3254e315bd4b03b756a90321e
AiLang-Author <smc.collins1977@gmail.com>
2025-07-08 17:10:03 -0400

    Delete ailang directory

--- ailang/__init__.py.py
+++
@@ -1,9 +0,0 @@
-# Module exports for AILANG compiler
-from .modules.arithmetic_ops import ArithmeticOps
-from .modules.fileio_ops import FileIOOps
-from .modules.control_flow import ControlFlow
-from .modules.memory_manager import MemoryManager
-from .modules.string_ops import StringOps
-from .modules.expression_compiler import ExpressionCompiler
-from .modules.code_generator import CodeGenerator
-from .modules.lowlevel_ops import LowLevelOps
